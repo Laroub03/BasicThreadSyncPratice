4a5c9dfc6c98c22b714f2c15150f9eeacb2600ef
Laroub03 <98460971+Laroub03@users.noreply.github.com>
2023-04-18 20:06:22 +0200

    Added comments

--- Program.cs
+++ Program.cs
@@ -1,63 +1,74 @@
-﻿using System;
+using System;
 using System.Threading;
 
 class Program
 {
-    static int counter = 0;
+    static int _number = 60;
     static object _lock = new object();
 
     static void Main()
     {
-        Thread increaseThread = new Thread(Increase);
-        Thread decreaseThread = new Thread(Decrease);
-        increaseThread.Start();
-        decreaseThread.Start();
-        increaseThread.Join();
-        decreaseThread.Join();
+        // Creating two new threads, one for printing stars and one for printing hashes
+        Thread starsThread = new Thread(PrintStars);
+        Thread hashesThread = new Thread(PrintHashes);
+
+        // Starting the two threads
+        starsThread.Start();
+        hashesThread.Start();
+
+        // Joining the two threads to the main thread
+        starsThread.Join();
+        hashesThread.Join();
     }
 
-    static void Increase()
+    // Method to print stars
+    static void PrintStars()
     {
+        while (true)
         {
-            while(true)
+            Monitor.Enter(_lock);
+            try
             {
-                Monitor.Enter(_lock);
-
-                try
+                // Looping through the number of stars to print
+                for (int i = 0; i < _number; i++)
                 {
-                counter += 2;
+                    Console.Write("*");
+                }
+
+                Console.Write(_number);
+                Console.WriteLine();
+                _number += 60;
                 Thread.Sleep(1000);
-                Console.WriteLine($"Increasing counter to {counter}");
-                }
-                finally
-                {
+            }
+            finally
+            {
                 Monitor.Exit(_lock);
-
-                }
             }
         }
     }
 
-    static void Decrease()
+    // Method to print hashes
+    static void PrintHashes()
     {
+        while (true)
         {
-            while(true)
+            Monitor.Enter(_lock);
+            try
             {
-                Monitor.Enter(_lock);
-
-                try
+                // Looping through the number of hashes to print
+                for (int i = 0; i < _number; i++)
                 {
-                    counter -= 1;
-                    Thread.Sleep(1000);
-                    Console.WriteLine($"Decreasing counter to {counter}");
+                    Console.Write("#");
                 }
-                finally
-                {
-                    Monitor.Exit(_lock);
-
-                }
+                Console.Write(_number);
+                Console.WriteLine();
+                _number += 60;
+                Thread.Sleep(1000);
+            }
+            finally
+            {
+                Monitor.Exit(_lock);
             }
         }
     }
 }
-

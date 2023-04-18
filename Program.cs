0e58749d3fc1fcab07142ce5c918dd8dde35b152
Laroub03 <ayoublaroub@gmail.com>
2023-04-18 10:17:18 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static int counter = 0;
+    static object _lock = new object();
+
+    static void Main()
+    {
+        Thread increaseThread = new Thread(Increase);
+        Thread decreaseThread = new Thread(Decrease);
+        increaseThread.Start();
+        decreaseThread.Start();
+        increaseThread.Join();
+        decreaseThread.Join();
+    }
+
+    static void Increase()
+    {
+        {
+            while(true)
+            {
+                Monitor.Enter(_lock);
+
+                try
+                {
+                counter += 2;
+                Thread.Sleep(1000);
+                Console.WriteLine($"Increasing counter to {counter}");
+                }
+                finally
+                {
+                Monitor.Exit(_lock);
+
+                }
+            }
+        }
+    }
+
+    static void Decrease()
+    {
+        {
+            while(true)
+            {
+                Monitor.Enter(_lock);
+
+                try
+                {
+                    counter -= 1;
+                    Thread.Sleep(1000);
+                    Console.WriteLine($"Decreasing counter to {counter}");
+                }
+                finally
+                {
+                    Monitor.Exit(_lock);
+
+                }
+            }
+        }
+    }
+}
+
